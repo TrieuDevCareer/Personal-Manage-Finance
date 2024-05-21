760fde8ccd40e757f2d3bd92814c43e2e7a2641a
TrieuDevCareer <lehaitrieu.dev.career@gmail.com>
2024-05-21 16:07:07 +0700

    update domain

--- client/src/util/domain.js
+++ client/src/util/domain.js
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
-  : process.env.NODE_ENV === "production" && "https://personal-manage-finance.vercel.app/";
+  : process.env.NODE_ENV === "production" && "https://personal-manage-finance.vercel.app";
