beb83b5fd4faab5432e86b47b68d86254d8678db
TrieuDevCareer <lehaitrieu.dev.career@gmail.com>
2024-05-22 14:18:50 +0700

    remove unuse code

--- client/src/components/misc/loadingProgess.js
+++ client/src/components/misc/loadingProgess.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 import "./loadingProgess.scss";
