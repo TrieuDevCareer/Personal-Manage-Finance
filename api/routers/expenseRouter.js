2a78c1c350056c6d69ed50d619d94b3abc86fa8e
TrieuDevCareer <lehaitrieu.dev.career@gmail.com>
2025-03-19 10:06:45 +0700

    update logic calculator day in month

--- api/routers/expenseRouter.js
+++ api/routers/expenseRouter.js
@@ -27,8 +27,8 @@ const getMonthLastDay = (year, month) => {
 };
 
 const calculateDaysUntilSalary = (currentDay, salaryDate) => {
-  if (salaryDate > currentDay) {
-    return salaryDate - currentDay;
+  if (salaryDate >= currentDay) {
+    return salaryDate - currentDay + 1;
   }
 
   const year = new Date().getFullYear();
