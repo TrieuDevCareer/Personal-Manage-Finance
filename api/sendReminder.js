7f654199f32a4ca55c0d2d9ff5265c3ddbd20229
TrieuDevCareer <lehaitrieu.dev.career@gmail.com>
2025-11-11 09:54:36 +0700

    test

---
+++ api/sendReminder.js
@@ -0,0 +1,16 @@
+import { User } from "./models/userModel";
+import commonUtil from "./commonUtils";
+
+export default async function handler(req, res) {
+    try {
+        const users = await User.find({ verifyMail: true });
+        for (const user of users) {
+            console.log(`Sending reminder email to ${user.email}`);
+            await commonUtil.sendReminderEmail(user.email, user.userName);
+        }
+        res.status(200).json({ success: true, message: "Reminders sent" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false });
+    }
+}
